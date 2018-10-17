09f1c1388d2b1a57732c535120afb2723889d2c0
itis <191908577@qq.com>
2018-10-17 18:42:50 +0800

    add class DelimiterExpression

--- src/main/java/xyz/lbzh/andy/expression/ast/DelimiterExpression.java
+++ src/main/java/xyz/lbzh/andy/expression/ast/DelimiterExpression.java
@@ -1,11 +1,25 @@
 package xyz.lbzh.andy.expression.ast;
 
-public class DelimiterExpression extends SymbolExpression{
+import xyz.lbzh.andy.expression.Context;
+import xyz.lbzh.andy.expression.Expression;
+import xyz.lbzh.andy.expression.ExpressionUtils;
+import xyz.lbzh.andy.expression.Name;
+
+public class DelimiterExpression extends TokenExpression{
     public DelimiterExpression(String value) {
         super(value);
     }
 
     public DelimiterExpression(String value, int lineNumber) {
         super(value, lineNumber);
+    }
+
+    @Override
+    public Expression eval(Context<Name, Expression> context) {
+        Expression rst = context.lookup(this);
+        while (ExpressionUtils.isSymbol(rst)) {
+            rst = context.lookup(rst.getName());
+        }
+        return rst;
     }
 }
