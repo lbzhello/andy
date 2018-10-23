8846d8c16e1a7019f378330b823fb59817030bbf
itis <191908577@qq.com>
2018-10-23 23:42:31 +0800

    update

--- src/main/java/xyz/lbzh/andy/Main.java
+++ src/main/java/xyz/lbzh/andy/Main.java
@@ -12,15 +12,15 @@ import org.springframework.context.annotation.*;
 @Import(AppConfig.class)
 public class Main {
     public static void main( String[] args ) throws Exception {
-        System.out.println("*****************************");
-        System.out.println("*  Application starting...  *");
-        System.out.println("*****************************");
+        System.out.println("*********************************");
+        System.out.println("* *  Application starting...  * *");
+        System.out.println("*********************************");
 
         Application.start(Main.class,args);
 
-        System.out.println("*****************************");
-        System.out.println("*  Application ended        *");
-        System.out.println("*****************************");
+        System.out.println("*********************************");
+        System.out.println("* *  Application ended        * *");
+        System.out.println("*********************************");
     }
 
 }
