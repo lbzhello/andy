912fc6726e99553751ddfed6a669b0425d70d13b
itis <191908577@qq.com>
2018-11-10 14:08:26 +0800

    add annotate on ExpressionArray

--- src/main/java/xyz/lbzh/andy/expression/ExpressionArray.java
+++ src/main/java/xyz/lbzh/andy/expression/ExpressionArray.java
@@ -6,20 +6,28 @@ package xyz.lbzh.andy.expression;
  * @see xyz.lbzh.andy.expression.runtime.ArrayMethodExpression
  */
 public interface ExpressionArray extends Expression {
+    //x -> y
     Expression map(Expression func);
 
+    //x -> nil
     Expression each(Expression func);
 
+    //x -> boolean
     Expression filter(Expression func);
 
+    //[x, y] -> [x, y -> z]
     Expression mapValues(Expression func);
 
+    //[x, y] -> z
     Expression reduce(Expression func);
 
+    //[[x, y] [x, z] ...] -> [[x, y, z] ...] -> [[x, [y, z] -> a] ...]
     Expression reduceByKey(Expression func);
 
+    //x -> [k x]
     Expression groupBy(Expression func);
 
+    //[[x, y] [x, z] ...] -> [[x, y, z] ...]
     Expression groupByKey();
 
     Expression first();
